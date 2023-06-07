07320707bf828dbee9f239baffc5a520e7ed7ac6
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-06-08 00:37:45 +0300

    alplinejs added to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import react from "@astrojs/react";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react()]
+  integrations: [vue(), react(), alpinejs()]
 });
