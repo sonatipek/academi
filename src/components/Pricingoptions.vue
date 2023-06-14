d4454872dd3e3f9bdccc9f211b28733cbf0299d7
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-06-14 16:59:30 +0300

    updayed egitimler.astro page

--- src/components/Pricingoptions.vue
+++ src/components/Pricingoptions.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="border border-primary-subtle rounded-4 w-50 d-flex flex-column justify-content-between">
+    <div class="border border-primary-subtle rounded-4 w-75 d-flex flex-column justify-content-between">
         <div class="bg-primary-subtle rounded-top-4 px-3 pb-2">
             <img :src="imgSrc" alt="card image" height="150" width="300">
             <h1 class="display-6">{{  title }}</h1>
         </div>
 
         <ul class="mt-3">
-            <li v-for="item in items" :key="item.id">{{ item.feature }}</li>
+            <li class="ms-4" v-for="item in items" :key="item.id">{{ item.feature }}</li>
         </ul>
 
         <div class="fw-bold fs-2 text-center">
@@ -26,6 +26,6 @@
 
 <style>
     div.border{
-        min-height: 380px;
+        min-height: 530px;
     }
 </style>
